c9c84f66f7d1b2fc6eb7c4420d6096cf2725e7c9
vientorepublic <markgod0419@gmail.com>
2024-10-06 09:49:04 +0900

    ✏️ chore: fix markdown to plain text function

--- src/app/utility/index.ts
+++ src/app/utility/index.ts
@@ -7,8 +7,9 @@ export class Utility {
     }
   }
   public stripMarkdown(input: string) {
-    const markdownPattern = /(\*\*|__)(.*?)\1|(\*|_)(.*?)\3|(`{1,3})(.*?)\5|!\[.*?\]\(.*?\)|\[.*?\]\(.*?\)|> .+|[-*+] \s*.*|#\s*.*|~~.*?~~|[\[\]()]/g;
-    return input.replace(markdownPattern, "").trim();
+    const markdownRegex = /[*_~`#+\-\[\]{}|<>]/g;
+    const plainText = input.replace(markdownRegex, "");
+    return plainText;
   }
   public escapeHTML(html: string): string {
     const escapeChars: { [key: string]: string } = {
